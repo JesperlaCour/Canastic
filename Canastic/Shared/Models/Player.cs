df9eb49fed72b33cc55616acc977f56d3c1bacf5
JesperlaCour <Jesper_laCour@me.com>
2021-11-18 13:09:09 +0100

    PlayerService up and running. Tested with swagger

---
+++ Canastic/Shared/Models/Player.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Shared.Models
+{
+    [BsonIgnoreExtraElements]
+    public class PlayerShort
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        [BsonElement("Name")]
+        public string PlayerName { get; set; }
+    }
+
+    [BsonIgnoreExtraElements]
+    public class Player
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        [BsonElement("Name")]
+        public string PlayerName { get; set; }
+
+        public DateTime PlayedSince { get; set; }
+
+        public int TotalWins { get; set; }
+    }
+}
---
+++ Canastic/UserService/Services/MongoService.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Driver;
+using PlayerService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using MongoDB.Bson;
+
+namespace PlayerService.Services
+{
+    public class MongoService : IMongoService
+    {
+        IMongoDatabase _db; 
+        public MongoService(string database)
+        {
+
+            MongoClient client = new(GetConnection().GetSection("ConnectionStrings").GetSection("MongoDBConn").Value);
+            _db = client.GetDatabase(database);
+        }
+
+        private IConfigurationRoot GetConnection()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            return builder;
+        }
+        public void DeleteRecord<T>(string table, Guid id)
+        {
+            var collection = _db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            collection.DeleteOne(filter);
+        }
+
+        public void InsertRecord<T>(string table, T record)
+        {
+            var collection = _db.GetCollection<T>(table);
+            collection.InsertOne(record);
+        }
+
+        public T LoadRecordByID<T>(string table, Guid id)
+        {
+            var collection = _db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            return collection.Find(filter).First();
+        }
+
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = _db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+        public void UpsertRecord<T>(string table, Guid id, T record)
+        {
+            BsonBinaryData binData = new(id, GuidRepresentation.Standard);
+            var collection = _db.GetCollection<T>(table);
+            var result = collection.ReplaceOne(
+                new BsonDocument("_id", binData),
+                record,
+                new ReplaceOptions { IsUpsert = true });
+        }
+    }
+}
